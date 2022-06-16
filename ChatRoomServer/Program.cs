481ed02c1cc8b2a1f7746c933bb46a010d8844c2
Kyle Tzeng <kyle1990913@gmail.com>
2022-06-16 17:55:32 +0800

    First commit

--- ChatRoomServer/Program.cs
+++ ChatRoomServer/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Net;
+using System.Net.Sockets;
 
 namespace ChatRoomServer
 {
@@ -6,7 +8,56 @@ namespace ChatRoomServer
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            const int port = 4099;
+            Console.WriteLine("==============");
+            var listener = new TcpListener(IPAddress.Any,port);
+            try
+            {
+                Console.WriteLine("Server start at port {0}", port);
+                listener.Start();
+
+                Console.WriteLine("Waiting for a connection... ");
+                var client = listener.AcceptTcpClient();
+
+                var address = client.Client.RemoteEndPoint.ToString();
+                Console.WriteLine("Client has connected from {0}", address);
+
+                while (true)
+                {
+                    Receive(client);
+                    System.Threading.Thread.Sleep(1000);
+                }
+
+                client.Close();
+                Console.WriteLine("Disconnect client {0}", address);
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine("SocketException: {0}", e);
+            }
+            finally
+            {
+                // Stop listening for new clients.
+                listener.Stop();
+                Console.WriteLine("Server shutdown");
+                Console.Read();
+            }
+        }
+        private static void Receive(TcpClient client)
+        {
+            var stream = client.GetStream();
+
+            var numBytes = client.Available;
+            if (numBytes == 0)
+            {
+                return;
+            }
+
+            var buffer = new byte[numBytes];
+            var bytesRead = stream.Read(buffer, 0, numBytes);
+
+            var request = System.Text.Encoding.Unicode.GetString(buffer).Substring(0, bytesRead);
+            Console.WriteLine("Text: " + request);
         }
     }
 }
